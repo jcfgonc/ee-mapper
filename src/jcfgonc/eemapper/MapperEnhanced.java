2f496b27e9c94da3462a7fd49472ea4d61f9e18f
João Gonçalves <jcgonc@gmail.com>
2020-12-04 19:47:41 +0000

    init

---
+++ src/jcfgonc/eemapper/MapperEnhanced.java
@@ -0,0 +1,127 @@
+package jcfgonc.eemapper;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.apache.commons.math3.random.Well44497a;
+
+import graph.DirectedMultiGraph;
+import graph.GraphReadWrite;
+import graph.StringGraph;
+import jcfgonc.eemapper.genetic.MapperGeneticOperations;
+import jcfgonc.eemapper.structures.MappingStructure;
+import jcfgonc.eemapper.structures.OrderedPair;
+import jcfgonc.genetic.GeneticAlgorithm;
+import structures.CSVWriter;
+import structures.Ticker;
+import utils.VariousUtils;
+
+public class MapperEnhanced {
+	public static void main(String[] args) throws Exception {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+		String filename = "..\\ConceptNet5\\kb\\conceptnet5v5.csv";
+		String mappingsFilename = VariousUtils.generateCurrentDateAndTimeStamp() + "_mappings.csv";
+
+		System.out.println("loading... " + filename);
+		StringGraph inputSpace = new StringGraph(1 << 24, 1 << 24, 1 << 24, 1 << 24);
+		GraphReadWrite.readCSV(filename, inputSpace);
+		inputSpace.showStructureSizes();
+
+		MapperGeneticOperations mgo = new MapperGeneticOperations(inputSpace);
+		CSVWriter csvw = new CSVWriter();
+		GeneticAlgorithm<MappingStructure<String, String>> ga = new GeneticAlgorithm<>(mgo, csvw);
+		ga.execute();
+		
+		MappingStructure<String, String> best = ga.getBestGenes();
+		System.out.println("mapping done, largest map has " + best.getMapping().size() + " pairs");
+		System.out.println("saving mappings to file " + mappingsFilename);
+		saveMappings(ga, new File(mappingsFilename));
+		
+		System.out.println("done.");
+		System.exit(0);
+	}
+
+	public static void saveMappings(GeneticAlgorithm<MappingStructure<String, String>> ga, File f) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new FileWriter(f), 1 << 16);
+		for (int i = 0; i < ga.getPopulationSize(); i++) {
+			MappingStructure<String, String> ms = ga.getGenes(i);
+			ms.toString(bw);
+			bw.newLine();
+		}
+		bw.close();
+	}
+
+	public static MappingStructure<String, String>[] getGeneticAlgorithmPopulation(GeneticAlgorithm<MappingStructure<String, String>> ga, int amount) {
+		int populationSize = ga.getPopulationSize();
+		@SuppressWarnings("unchecked")
+		MappingStructure<String, String>[] population = new MappingStructure[amount];
+		for (int i = 0; i < amount; i++) {
+			// ga population is in ascending order, revert so that the first element is the highest
+			population[i] = ga.getGenes(populationSize - i - 1);
+		}
+		return population;
+	}
+
+	/**
+	 * runs a single stochastic execution of the EEmapping algorithm
+	 * 
+	 * @param filename
+	 * @throws Exception
+	 */
+	public void executeSingle(String filename) throws Exception {// one mappingset per chromosome
+		Ticker ticker = new Ticker();
+
+		System.out.println("loading... " + filename);
+		ticker.getTimeDeltaLastCall();
+		StringGraph inputSpace = new StringGraph(1 << 24, 1 << 24, 1 << 24, 1 << 24);
+		GraphReadWrite.readCSV(filename, inputSpace);
+		System.out.println(ticker.getTimeDeltaLastCall());
+		inputSpace.showStructureSizes();
+		System.out.println("-------");
+
+		DirectedMultiGraph<OrderedPair<String>, String> pairGraph = new DirectedMultiGraph<>();
+		Well44497a random = new Well44497a();
+		OrderedPair<String> refPair = MappingAlgorithms.getRandomConceptPair(inputSpace, random);
+		MappingAlgorithms.createIsomorphism(inputSpace, pairGraph, random, refPair, Integer.MAX_VALUE);
+
+		MappingStructure<String, String> mapStruct = new MappingStructure<>();
+		mapStruct.setPairGraph(pairGraph);
+		// mapStruct.setRandom(recordingRandom);
+		mapStruct.setRefPair(refPair);
+		System.out.println("mapping done: " + pairGraph.vertexSet().size() + " pairs");
+		mapStruct.writeTGF(VariousUtils.generateCurrentDateAndTimeStamp() + "_mapping.tgf");
+	}
+
+	/**
+	 * executes the GA version of the EEmapper, ready to be invoked as a Web Service.
+	 * 
+	 * @param inputSpace textual description (source,relation,target) of the semantic graph to find mappings in
+	 * @param csvw       if null no execution log is created
+	 * @return
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws UnsupportedLookAndFeelException
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static MappingStructure<String, String> executeGeneticAlgorithm(StringGraph inputSpace, CSVWriter csvw)
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, IOException, InterruptedException {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+		MapperGeneticOperations mgo = new MapperGeneticOperations(inputSpace);
+		GeneticAlgorithm<MappingStructure<String, String>> ga = new GeneticAlgorithm<>(mgo, csvw);
+		ga.execute();
+		MappingStructure<String, String> best = ga.getBestGenes();
+
+		System.out.println("mapping done: " + best.getMapping().size() + " pairs");
+		return best;
+	}
+
+}
---
+++ src/jcfgonc/eemapper/genetic/MapperGeneticOperations.java
@@ -0,0 +1,137 @@
+package jcfgonc.eemapper.genetic;
+
+import java.util.ArrayList;
+
+import org.apache.commons.math3.random.RandomGenerator;
+
+import graph.GraphAlgorithms;
+import graph.StringGraph;
+import jcfgonc.eemapper.MappingAlgorithms;
+import jcfgonc.eemapper.structures.MappingStructure;
+import jcfgonc.eemapper.structures.OrderedPair;
+import jcfgonc.genetic.operators.GeneticOperations;
+import utils.VariousUtils;
+
+public class MapperGeneticOperations implements GeneticOperations<MappingStructure<String, String>> {
+
+	private static final double BRIDGE_JUMPING_RANGE = 3;
+	private static final double LOCAL_JUMP_PROBABILITY = 0.88;
+	private static final double MUTATION_JUMP_PROBABILITY_POWER = 2.5;
+	private static final int DEEPNESS_LIMIT = -1;
+	private StringGraph inputSpace;
+	private ArrayList<String> vertexSetAsList;
+
+	public MapperGeneticOperations(StringGraph inputSpace) {
+		this.inputSpace = inputSpace;
+		this.vertexSetAsList = new ArrayList<>(inputSpace.getVertexSet());
+	}
+
+	@Override
+	public MappingStructure<String, String> createGeneCopy(MappingStructure<String, String> genes, boolean soonChanged) {
+		MappingStructure<String, String> mappingStructure = new MappingStructure<String, String>();
+		mappingStructure.setRefPair(new OrderedPair<>(genes.getRefPair())); // very important
+		// shallow copy
+		if (soonChanged) {
+		} else { // deep copy
+			mappingStructure.setPairGraph(genes.getPairGraph());
+		}
+		return mappingStructure;
+	}
+
+	@Override
+	public void crossover(MappingStructure<String, String> parent0, MappingStructure<String, String> parent1, MappingStructure<String, String> offSpring0,
+			MappingStructure<String, String> offSpring1, RandomGenerator random) {
+
+	}
+
+	@Override
+	public double evaluateFitness(MappingStructure<String, String> mapStruct) {
+		return mapStruct.getMapping().size();
+	}
+
+	@Override
+	public MappingStructure<String, String> initializeGenes(RandomGenerator random) {
+		MappingStructure<String, String> mappingStruct = new MappingStructure<>();
+		// get a random concept pair
+		OrderedPair<String> refPair = MappingAlgorithms.getRandomConceptPair(inputSpace, random);
+		mappingStruct.setRefPair(refPair);
+
+		MappingAlgorithms.updateMappingGraph(inputSpace, mappingStruct, DEEPNESS_LIMIT, random);
+		return mappingStruct;
+	}
+
+	@Override
+	public MappingStructure<String, String> mutateGenes(MappingStructure<String, String> genes, RandomGenerator random) {
+		OrderedPair<String> refPair = genes.getRefPair();
+		String leftElement = refPair.getLeftElement();
+		String rightElement = refPair.getRightElement();
+
+		// unalign/rearrange a new refpair from the existing
+		// -------------------------------------------------
+		// offset locally?
+		if (random.nextDouble() < LOCAL_JUMP_PROBABILITY) {
+			// do a random walk on either left or right concepts (or both)
+			if (random.nextBoolean()) {
+				double r = Math.pow(random.nextDouble(), MUTATION_JUMP_PROBABILITY_POWER);
+				int hops = (int) Math.ceil(r * BRIDGE_JUMPING_RANGE);
+				leftElement = GraphAlgorithms.getVertexFromRandomWalk(random, leftElement, inputSpace, hops);
+			}
+			if (random.nextBoolean()) {
+				double r = Math.pow(random.nextDouble(), MUTATION_JUMP_PROBABILITY_POWER);
+				int hops = (int) Math.ceil(r * BRIDGE_JUMPING_RANGE);
+				rightElement = GraphAlgorithms.getVertexFromRandomWalk(random, rightElement, inputSpace, hops);
+			}
+
+			// prevent left and right from being equals
+			while (leftElement.equals(rightElement)) {
+				double r = Math.pow(random.nextDouble(), MUTATION_JUMP_PROBABILITY_POWER);
+				int hops = (int) Math.ceil(r * BRIDGE_JUMPING_RANGE);
+				rightElement = GraphAlgorithms.getVertexFromRandomWalk(random, rightElement, inputSpace, hops);
+			}
+		}
+		// offset globally
+		else {
+			// do a random shift to far away on either left or right concepts (or both)
+			if (random.nextBoolean()) {
+				leftElement = VariousUtils.getRandomElementFromCollection(vertexSetAsList, random);
+			}
+			if (random.nextBoolean()) {
+				rightElement = VariousUtils.getRandomElementFromCollection(vertexSetAsList, random);
+			}
+
+			// prevent left and right from being equals
+			while (leftElement.equals(rightElement)) {
+				rightElement = VariousUtils.getRandomElementFromCollection(vertexSetAsList, random);
+			}
+		}
+
+		// if (refPair.getLeftElement().equals(leftElement) && //
+		// refPair.getRightElement().equals(rightElement)) {
+		// System.err.println("reference pair did not change");
+		// }
+		// store refpair back in the gene
+		genes.setRefPair(new OrderedPair<String>(leftElement, rightElement));
+		MappingAlgorithms.updateMappingGraph(inputSpace, genes, DEEPNESS_LIMIT, random);
+		return genes;
+	}
+
+	@Override
+	public MappingStructure<String, String> repairGenes(MappingStructure<String, String> genes, RandomGenerator random) {
+		return genes;
+	}
+
+	@Override
+	public boolean useCrossover() {
+		return false;
+	}
+
+	@Override
+	public boolean useGeneRepair() {
+		return false;
+	}
+
+	@Override
+	public boolean useMutation() {
+		return true;
+	}
+}
